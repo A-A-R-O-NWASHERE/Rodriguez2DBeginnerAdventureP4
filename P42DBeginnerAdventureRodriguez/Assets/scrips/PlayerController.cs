988783f7fa04688c0ec639ec01b4ab11dc5252df
A-A-R-O-NWASHERE <768335@student.dusd.net>
2025-11-10 11:58:54 -0800

    update player movement
    we updating the code so the play can move

--- P42DBeginnerAdventureRodriguez/Assets/scrips/PlayerController.cs
+++ P42DBeginnerAdventureRodriguez/Assets/scrips/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,8 +14,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        float horizontal = 0.0f;
+
+        if(Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        {
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+                    Ver = 1.0f;
+        }
+        Debug.Log(horizontal)
+
+        vector2 position = transform.position;
+        position.x = position.x + 0.001f;
+        position.x = position.x + 0.001f;
         transform.position = position;
     }
 }
