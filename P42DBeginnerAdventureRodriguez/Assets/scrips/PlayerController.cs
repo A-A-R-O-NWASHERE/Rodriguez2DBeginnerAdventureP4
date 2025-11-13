05d6b443733e8a9c3e1c3fd452598e113909c170
A-A-R-O-NWASHERE <768335@student.dusd.net>
2025-11-13 11:27:53 -0800

    updated movement
    i fix my error and finish unit 4 now on unity five "Create Input Actions for player character movement"

--- P42DBeginnerAdventureRodriguez/Assets/scrips/PlayerController.cs
+++ P42DBeginnerAdventureRodriguez/Assets/scrips/PlayerController.cs
@@ -1,34 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
+    public InputAction LeftAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = 0.0f;
-
-        if(Keyboard.current.leftArrowKey.isPressed)
+        if (Keyboard.current.leftArrowKey.isPressed)
         {
-            horizontal = -1.0f;
-        {
+            horizontal = 1.0f;
+        }
         else if (Keyboard.current.rightArrowKey.isPressed)
         {
-                    Ver = 1.0f;
+            horizontal = 1.0f;
         }
-        Debug.Log(horizontal)
+        Debug.Log(horizontal);
 
-        vector2 position = transform.position;
-        position.x = position.x + 0.001f;
-        position.x = position.x + 0.001f;
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        Debug.Log(vertical);
+
+
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
+
 }
